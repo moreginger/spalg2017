6fdfa0c1617cb9e7eb69c424794dee640d6d7264
Tim Moore <tmm@corefiling.com>
2017-10-16 21:00:52 +0100

    Generic impl of fast gaussian
    http://rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
    
    Except doesn't have a gaussian decay function but it does do the linear subsampling via texture2d calls.

--- shine/gaussianblur.lua
+++ shine/gaussianblur.lua
@@ -23,31 +23,71 @@ SOFTWARE.
 ]]--
 
 -- unroll convolution loop
-local function build_shader(taps)
-	local code = {[[
-		extern vec2 direction;
-		uniform sampler2D tex0;
-		vec4 effect(vec4 color, Image texture, vec2 tc, vec2 sc)
-		{
-		vec3 colOut = vec3( 0.0f );]]}
-
-	local steps = (taps + 1) / 2
-
-	local one_by_sigma_sq = steps > 0 and 1 / (steps * steps) or 1
-	local norm = math.exp(0)
-
-	local tmpl = 'colOut += %f * ( texture2D( tex0, tc + %f * direction ).xyz + texture2D( tex0, tc - %f * direction ).xyz );'
-
-	code[#code+1] = ('colOut += %f * texture2D( tex0, tc).xyz;'):format(norm)
-	for i = 1, steps, 1 do
-		local coeff = math.exp(-.5 * i*i * one_by_sigma_sq)
-		norm = norm + coeff
-		print(coeff)
-		code[#code+1] = tmpl:format(coeff, i, i)
+local function build_shader(sigma)
+	local sigma = math.floor(sigma)
+	if sigma < 1 or sigma % 2 ~= 1 then
+	    error(('Sigma must be >0 and odd. Was %d.'):format(sigma))
 	end
-	code[#code+1] = ('return vec4(colOut, 1.0f) * vec4(%f);}'):format(0.5 / norm)
+	local steps = (math.floor(sigma) + 1) / 2
 
-	return love.graphics.newShader(table.concat(code))
+	local g_offset = {}
+	local g_weight = {}
+	for i = 1, steps, 1 do
+		g_offset[i] = i - 1
+		-- TODO gaussian function
+		g_weight[i] = 1 / ( i * i )
+	end
+
+	local offset = {}
+	local weight = {}
+	for i = #g_weight, 2, -2 do
+		local oA, oB = g_offset[i], g_offset[i - 1]
+		local wA, wB = g_weight[i], g_weight[i - 1]
+		offset[#offset + 1] = (oA * wA + oB * wB) / (wA + wB)
+		weight[#weight + 1] = wA + wB
+	end
+
+	-- TODO centre
+
+
+	local code = {[[
+extern vec2 direction;
+uniform sampler2D tex0;
+vec4 effect(vec4 color, Image texture, vec2 tc, vec2 sc)
+{
+    vec3 colOut = vec3( 0.0f );
+]]}
+
+	local tmpl = '    colOut += %f * ( texture2D( tex0, tc + %f * direction ).xyz + texture2D( tex0, tc - %f * direction ).xyz );\n'
+
+	local norm = 0
+	for i = 1, #offset, 1 do
+		local offset = offset[i]
+		local weight = weight[i]
+		norm = norm + weight * 2
+		code[#code+1] = tmpl:format(weight, offset, offset)
+	end
+	print(#g_weight)
+	if #g_weight % 2 == 0 then
+		print('case 1')
+		local weight = g_weight[1]
+		norm = norm + weight
+		code[#code+1] = ('    colOut += %f * texture2D( tex0, tc).xyz;'):format(weight)
+	else
+		-- TODO avoid duplicating code
+		print('case 2')
+		local oA, oB = g_offset[2], g_offset[1];
+		local wA, wB = g_weight[2], g_weight[1]
+		local weight = wA + wB
+		norm = norm + weight * 2
+		local offset = (oA * wA + oB * wB) / (wA + wB)
+		code[#code+1] = tmpl:format(weight, offset, offset)
+	end
+	code[#code+1] = ('    return vec4(colOut, 1.0f) * vec4(%f);}'):format(1 / norm)
+
+	local shader = table.concat(code)
+	print(shader)
+	return love.graphics.newShader(shader)
 end
 
 return {
@@ -55,7 +95,7 @@ description = "Fast Gaussian blur shader",
 
 new = function(self)
 	self.canvas_h, self.canvas_v = love.graphics.newCanvas(), love.graphics.newCanvas()
-	self.shader = build_shader(1)
+	self.shader = build_shader(7)
 	self.shader:send("direction",{1.0,0.0})
 end,
 
@@ -88,7 +128,7 @@ draw = function(self, func, ...)
 end,
 
 set = function(self, key, value)
-	if key == "taps" then
+	if key == "sigma" then
 		self.shader = build_shader(tonumber(value))
 	else
 		error("Unknown property: " .. tostring(key))
