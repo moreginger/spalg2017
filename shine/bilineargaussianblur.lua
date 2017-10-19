96d3e7441a582aa651daf44a20cb50832f32d555
Tim Moore <tmm@corefiling.com>
2017-10-19 23:43:25 +0100

    Sample mid-pixels
    The previous algorithm, whilst more correct in terms of the relative contributions according the the decay function, was poor when dealing with high frequency (i.e. 1 pixel) details as it modulated the intensity on alternating pixels. This was very clear if the decay function was disabled.

--- shine/bilineargaussianblur.lua
+++ shine/bilineargaussianblur.lua
@@ -24,7 +24,7 @@ SOFTWARE.
 
 -- Bilinear Gaussian blur filter as detailed here: http://rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
 -- Produces near identical results to a standard Gaussian blur by using sub-pixel sampling,
--- this allows us to do ~1/2 the number of pixel lookups.
+-- this allows us to do ~1/2 the number of lookups.
 
 -- unroll convolution loop
 local function build_shader(taps, offset, sigma)
@@ -40,25 +40,28 @@ local function build_shader(taps, offset, sigma)
 	-- Calculate gaussian function.
 	local g_offsets = {}
 	local g_weights = {}
+	local norm = 0
 	for i = 1, steps, 1 do
-		local offset = i - 1
-		g_offsets[i] = offset
+		g_offsets[i] = offset * (i - 1)
 
 		-- We don't need to include the constant part of the gaussian function as we normalize later.
 		-- 1 / math.sqrt(2 * sigma ^ math.pi) * math.exp(-0.5 * ((offset - 0) / sigma) ^ 2 )
-		g_weights[i] = math.exp(-0.5 * (offset - 0) ^ 2 * 1 / sigma ^ 2 )
+		weight = math.exp(-0.5 * (g_offsets[i] - 0) ^ 2 * 1 / sigma ^ 2 )
+		g_weights[i] = weight
+		norm = norm + (i == 1 and weight or weight * 2)
 	end
 
 	-- Calculate offsets and weights for sub-pixel samples.
 	local offsets = {}
 	local weights = {}
+	local gnorm = 0
 	for i = #g_weights, 2, -2 do
 		local oA, oB = g_offsets[i], g_offsets[i - 1]
 		local wA, wB = g_weights[i], g_weights[i - 1]
-		wB = i ~=2 and wB or wB / 2 -- On final tap the middle is getting sampled twice so half weight.
-		local weight = wA + wB
-		offsets[#offsets + 1] = (oA * wA + oB * wB) / weight
-		weights[#weights + 1] = weight
+		wB = i - 1 == 1 and wB / 2 or wB -- On final tap the middle is getting sampled twice so half weight.
+		offsets[#offsets + 1] = (oA + oB) / 2
+		weights[#weights + 1] = wA + wB
+		gnorm = gnorm + wA + wB
 	end
 
 	local code = {[[
@@ -66,25 +69,23 @@ extern vec2 direction;
 uniform sampler2D tex0;
 vec4 effect(vec4 color, Image texture, vec2 tc, vec2 sc) {]]}
 
-    local norm = 0
 	if #g_weights % 2 == 0 then
-		code[#code+1] =  'vec4 c = vec4( 0.0f );'
+		code[#code+1] =  'vec3 c = vec3( 0.0f );'
 	else
 		local weight = g_weights[1]
-		norm = norm + weight
-		code[#code+1] = ('vec4 c = %f * texture2D( tex0, tc ).xyzw;'):format(weight)
+		code[#code+1] = ('vec3 c = %f * texture2D( tex0, tc ).rgb;'):format(weight)
 	end
 
-	local tmpl = 'c += %f * ( texture2D( tex0, tc + %f * direction ).xyzw + texture2D( tex0, tc - %f * direction ).xyzw );\n'
+	local tmpl = 'c += %f * ( texture2D( tex0, tc + %f * direction ).rgb + texture2D( tex0, tc - %f * direction ).rgb );\n'
 	for i = 1, #offsets, 1 do
 		local offset = offsets[i]
 		local weight = weights[i]
-		norm = norm + weight * 2
 		code[#code+1] = tmpl:format(weight, offset, offset)
 	end
-	code[#code+1] = ('return c * vec4(%f) * color; }'):format(1 / norm)
+	code[#code+1] = ('return %f * vec4(c, 1.0f) * color; }'):format(1 / norm)
 
 	local shader = table.concat(code)
+	print(shader)
 	return love.graphics.newShader(shader)
 end
 
@@ -121,7 +122,7 @@ draw = function(self, func, ...)
 	-- second pass (vertical blur)
 	self.shader:send('direction', {0, 1 / love.graphics.getHeight()})
 	love.graphics.draw(self.canvas_v, 0,0)
-
+	
 	-- restore blendmode, shader and canvas
 	love.graphics.setBlendMode(b)
 	love.graphics.setShader(s)
