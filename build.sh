4752c7104c4d86851577939e65794ccdd06f590e
Tim Moore <moreginger@warpmail.net>
2021-07-27 09:05:30 +0100

    Improve build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-zip -r9 spalg.love *.lua hc hump moonshine resources states tween
+if [ -z ${1+x} ]; then echo "output directory unset"; else echo "output directory set to '${1}'"; fi
+
+zip -r9 ${1}/spalg.love *.lua hc hump resources states tween
